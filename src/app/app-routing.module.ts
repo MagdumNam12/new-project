7b213b5b2da506f35c356cc762e6708af5fcd1ae
MagdumNam12 <namrata.magdum@flixirsolutions.com>
2022-07-07 10:02:36 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DescriptionBlockComponent } from './description-block/description-block.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MapComponent } from './map/map.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PiechartComponent } from './piechart/piechart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'header',
+    component:HeaderComponent,
+    },
+    {
+      path:'navbar',
+      component:NavbarComponent,
+      },
+      {
+        path:'description-block',
+        component:DescriptionBlockComponent,
+        },
+        {
+          path:'map',
+          component:MapComponent,
+          },
+          {
+            path:'piechart',
+            component:PiechartComponent,
+            },
+            {
+              path:'footer',
+              component:FooterComponent,
+              },
+
+    {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'page'
+    }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DescriptionBlockComponent } from './description-block/description-block.component';
+import { MapComponent } from './map/map.component';
+import { FooterComponent } from './footer/footer.component';
+import { PiechartComponent } from './piechart/piechart.component';
+import { ChartsModule } from 'ng2-charts';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    DescriptionBlockComponent,
+    MapComponent,
+    FooterComponent,
+    PiechartComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
